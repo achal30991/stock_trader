17b5b658c69c04d7750e058220baa403c7aba655
Raghu Betina <raghu@firstdraft.com>
2019-06-12 21:09:51 +0000

    Direct association between stocks and upvotes

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,9 @@
 class Stock < ApplicationRecord
   # Direct associations
 
+  has_many   :upvotes,
+             :dependent => :destroy
+
   belongs_to :portfolio,
              :required => false
 
--- spec/models/stock_spec.rb
+++ spec/models/stock_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Stock, type: :model do
   
     describe "Direct Associations" do
 
+    it { should have_many(:upvotes) }
+
     it { should belong_to(:portfolio) }
 
     end
